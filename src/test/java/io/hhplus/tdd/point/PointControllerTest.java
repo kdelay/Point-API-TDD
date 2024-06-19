cf7f427062485bd727d6600bdb94989a4f1edec6
kdelay <kdelay20@gmail.com>
2024-06-19 20:42:54 +0900

    test: #4 포인트 조회 controller

--- src/test/java/io/hhplus/tdd/point/PointControllerTest.java
+++ src/test/java/io/hhplus/tdd/point/PointControllerTest.java
@@ -14,6 +14,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -29,6 +30,39 @@ class PointControllerTest {
 
     @MockBean
     private PointService pointService;
+
+    // ---------------------------------------------------------------------------
+
+    // /point/{id}
+    @Test
+    @DisplayName("GET 포인트 조회(아이디 O)")
+    void selectTest_isExistsId() throws Exception {
+
+        //mocking
+        long id = 1L;
+        long amount = 100L;
+        UserPoint userPoint = new UserPoint(id, amount, System.currentTimeMillis());
+
+        when(pointService.select(id)).thenReturn(userPoint);
+
+        mockMvc.perform(get("/point/{id}", id))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("GET 포인트 조회(아이디 X)")
+    void selectTest_isEmptyId() throws Exception {
+
+        //mocking
+        long id = -1L;
+
+        when(pointService.select(id)).thenThrow(new IllegalArgumentException("아이디가 존재하지 않습니다."));
+
+        mockMvc.perform(get("/point/{id}", id))
+                .andExpect(status().isInternalServerError());
+    }
+
+    // ---------------------------------------------------------------------------
 
     // /point/{id}/charge
     @Test
